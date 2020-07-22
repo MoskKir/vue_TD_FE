88a94620b632e60e097c182c645ea4ae5af5b703
MoskKir <47816850+MoskKir@users.noreply.github.com>
2020-07-22 19:18:23 +0300

    feat: add animations to components

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,56 +1,58 @@
 <template>
-    <div
-      class="container"
-    > 
-      
-          <SigninForm></SigninForm>
+    <div class="container" ref="signinContainer">
+        <SigninForm></SigninForm>
 
-          <br>
-          <div class="description">
+        <br />
+        <div class="description">
             <p>
-              Don't have an account?
-              <a>
-                <router-link to="/auth/signup">
-                  Sign up for TODO Aplication
-                </router-link>                 
-              </a>
+                Don't have an account?
+                <a>
+                    <router-link to="/auth/signup">
+                        Sign up for TODO Aplication
+                    </router-link>
+                </a>
             </p>
-            
-          </div>
-          <br>
-
-    </div> 
+        </div>
+        <br />
+    </div>
 </template>
 
 <script>
-import SigninForm from '@/components/SigninForm.vue';
+import SigninForm from "@/components/SigninForm.vue";
+import Animation from "../helpers/animations";
 
 export default {
-  name: 'Signin',
-  components: {
-    SigninForm,
-  }
-}
+    name: "Signin",
+    components: {
+        SigninForm,
+    },
+    data: () => ({
+        signinContainer: null,
+    }),
+    mounted() {
+        this.signinContainer = this.$refs.signinContainer;
 
+        Animation.from(this.signinContainer);
+    },
+    beforeRouteLeave(to, from, next) {
+        Animation.out(this.signinContainer, next);
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-
-  .container {
+.container {
     margin-top: 50px;
     width: calc(100% - 40%);
     max-width: 800px;
     height: fit-content;
     padding: 20px;
-    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 
-                    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 
-                    0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-                  
-  }
+    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+        0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+}
 
-  p {
+p {
     margin: 0 auto;
     text-align: center;
-  }
-
+}
 </style>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,25 +1,50 @@
 <template>
-  <div class="todo-container">
-    <TodoTable></TodoTable>
-  </div>
+    <div class="todo-container" ref="todoContainer">
+        <TodoTable></TodoTable>
+    </div>
 </template>
 
 <script>
-import TodoTable from '../components/TodoTable';
+import { TimelineLite } from "gsap";
+
+import TodoTable from "../components/TodoTable";
+import Animation from "../helpers/animations";
 
 export default {
-  name: 'Todos',
-  components: {
-    TodoTable,
-  },
-  data: () => ({
+    name: "Todos",
+    components: {
+        TodoTable,
+    },
+    data: () => ({}),
+    mounted() {
+        const { todoContainer } = this.$refs;
+        const timeline = new TimelineLite();
 
-  }),
-}
+        timeline.fromTo(
+            todoContainer,
+            {
+                y: -500,
+            },
+            {
+                y: 75,
+            }
+        );
+        timeline.to(todoContainer, {
+            y: 0,
+        });
+    },
+    beforeRouteLeave(to, from, next) {
+        console.log("before destroy");
+        const { todoContainer } = this.$refs;
+
+        Animation.out(todoContainer, next);
+    },
+};
 </script>
 
 <style lang="scss">
-  .todo-container {
+.todo-container {
     margin: 0 auto;
-  }
+    height: fit-content;
+}
 </style>
